177d77e179ee1c982d3031024458798a66e21ab6
Bayutopo <127712088+Bayutopo@users.noreply.github.com>
2023-03-21 09:05:47 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_class_object_taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            taxi taxi = new taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.Onduty = true;
+            taxi.NumPassanger = 10;
+
+            taxi.TaxiInfo();
+
+            Console.WriteLine();
+
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_class_object_taxi
+{
+    public class taxi
+    {
+
+        public string DriverName { get; set; }
+        public Boolean Onduty { get; set; }
+        public int NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (Onduty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else if (Onduty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Num Passanger : {0}", NumPassanger);
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
